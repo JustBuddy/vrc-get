5cd3e98ccbc841d328efc399565603faf71c3523
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2024-04-22 16:48:41 +0200

    Backported Dark-Mode Hack

--- vrc-get-gui/tailwind.config.ts
+++ vrc-get-gui/tailwind.config.ts
@@ -8,6 +8,23 @@ const config: Config = {
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+		colors: {
+			'white': '#212121',
+			'font-white': '#FFFFFF',
+			'blue-500': '#BABABA',
+			'blue-100': '#494949',
+			'gray': '#8492a6',
+			'gray-900': '#FDDA0D',
+			'gray-light': '#d3dce6',
+			'blue-gray-50': '#212121',
+			'blue-gray-100': '#FDDA0D',
+			'blue-gray-500': '#BABABA',
+			'blue-gray-600': '#BABABA',
+			'blue-gray-700': '#BABABA',
+			'blue-gray-800': '#BABABA',
+			'blue-gray-900': '#BABABA',
+			'green-500': '#BABABA',
+		},
 		extend: {
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
